9a00245825175491684ec7e92d2424d795ac4d7d
anandijitendra23 <anandijitendra23@gmail.com>
2026-06-22 22:45:39 +0530

    Add files via upload

---
+++ DisplayDatatype1.py
@@ -0,0 +1,18 @@
+from sys import getsizeof
+x = 100
+print("x = ",x)
+print("Datatype : ",type(x))
+print("Memory Address : ",id(x))
+print("Size in bytes : ",getsizeof(x))
+
+name = "Python"
+print("name = ",name)
+print("Datatype : ",type(name))
+print("Memory Address : ",id(name))
+print("Size in bytes : ",getsizeof(name))
+
+marks=[50,60,55,75]
+print("marks = ",marks)
+print("Datatype : ",type(marks))
+print("Memory Address : ",id(marks))
+print("Size in bytes : ",getsizeof(marks))
